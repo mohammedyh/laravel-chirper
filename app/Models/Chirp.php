a187273866f4494dae0a00f06c32ff7061995b08
mohammedyh <mohammedyousufhamid@gmail.com>
2024-01-23 23:19:21 +0000

    Add notification email for new chirps

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -16,6 +17,10 @@ class Chirp extends Model
      * @var array<int, string>
      */
     protected $fillable = ['message'];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
 
     public function user(): BelongsTo
     {
